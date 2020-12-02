e39988f1f4600a8235064f40b5351ee563b0e447
dongmingcan <dongmingcan@baidu.com>
2020-12-02 12:21:49 +0800

    duplicate row 2

--- duplication_row.go
+++ duplication_row.go
@@ -7,7 +7,37 @@ import (
 )
 
 func main() {
-	statInput1()
+	statInput2()
+}
+
+func statInput2() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, file := range files {
+			f, err := os.Open(file)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "statInput2 err: %v/n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
 }
 
 func statInput1() {
@@ -21,6 +51,7 @@ func statInput1() {
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Printf("%v\t%s\n", n, line)
 		}
 	}
 }
